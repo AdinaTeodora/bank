ad0df8f1ced45c395699ebbb0096e15ebb76850d
Adina Teodora <teodora_ardeleanu@yahoo.com>
2022-03-23 23:13:53 +0000

    Added logic for opening and managing bank account actions

--- src/bank_server.erl
+++ src/bank_server.erl
@@ -12,24 +12,49 @@
 -behaviour(gen_server).
 
 %% API
--export([start_link/0]).
+-export([start_link/0, balance/0, transactions/0, charge/1, withdraw/1, deposit/1]).
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
--define(SERVER, ?MODULE).
+-type amount() :: integer().
+-type action() :: charge | withdraw | deposit.
 
--record(state, {}).
+%% Default user's initial balance to 100 Bambeuros due to promotion on signup.
+-record(account, {
+  balance      = 100 :: amount(),
+  transactions = []  :: [{action(), amount()}]
+}).
 
 %%%===================================================================
 %%% API
 %%%===================================================================
 
-%% @doc Spawns the server and registers the local name (unique)
--spec(start_link() ->
-  {ok, Pid :: pid()} | ignore | {error, Reason :: term()}).
+%% @doc Start / Create a bank account for a user.
+
 start_link() ->
-  gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+  gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+-spec balance() -> Balance :: amount().
+balance() ->
+  gen_server:call(?MODULE, balance).
+
+-spec transactions() -> Transactions :: [{action(), amount()}].
+transactions() ->
+  io:format("Showing transactions in order of most recent: ~n"),
+  gen_server:call(?MODULE, transactions).
+
+-spec charge(amount()) -> Balance :: amount().
+charge(Amount) ->
+  gen_server:call(?MODULE, {charge, Amount}).
+
+-spec withdraw(amount()) -> Balance :: amount().
+withdraw(Amount) ->
+  gen_server:call(?MODULE, {withdraw, Amount}).
+
+-spec deposit(amount()) -> Balance :: amount().
+deposit(Amount) ->
+  gen_server:call(?MODULE, {deposit, Amount}).
 
 %%%===================================================================
 %%% gen_server callbacks
@@ -37,61 +62,70 @@ start_link() ->
 
 %% @private
 %% @doc Initializes the server
--spec(init(Args :: term()) ->
-  {ok, State :: #state{}} | {ok, State :: #state{}, timeout() | hibernate} |
-  {stop, Reason :: term()} | ignore).
 init([]) ->
-  {ok, #state{}}.
+  {ok, #account{}}.
 
 %% @private
 %% @doc Handling call messages
--spec(handle_call(Request :: term(), From :: {pid(), Tag :: term()},
-    State :: #state{}) ->
-  {reply, Reply :: term(), NewState :: #state{}} |
-  {reply, Reply :: term(), NewState :: #state{}, timeout() | hibernate} |
-  {noreply, NewState :: #state{}} |
-  {noreply, NewState :: #state{}, timeout() | hibernate} |
-  {stop, Reason :: term(), Reply :: term(), NewState :: #state{}} |
-  {stop, Reason :: term(), NewState :: #state{}}).
-handle_call(_Request, _From, State = #state{}) ->
+handle_call(balance, _From, Account = #account{balance = Balance}) ->
+  {reply, io:format("Balance is at ~p.~n", [Balance]), Account};
+handle_call(transactions, _From, Account = #account{transactions = []}) ->
+  {reply, io:format("No transactions made yet.~n"), Account};
+handle_call(transactions, _From, Account = #account{transactions = Transactions}) ->
+  FormattedTransactions = lists:foreach(fun({Action, Amount}) ->
+    io:format("~p -> ~p Bambeuros;~n", [Action, Amount])
+  end, Transactions),
+  {reply, FormattedTransactions, Account};
+handle_call({charge, Amount}, _From, Account) when Amount =< 0 ->
+  {reply, 0, Account};
+handle_call({charge, Amount}, _From, Account = #account{balance = Balance, transactions = Transactions}) ->
+  case Balance > Amount of
+    true  ->
+      NewAccount = Account#account{balance = Balance - Amount, transactions = [{charge, Amount} | Transactions]},
+      NewBalance = NewAccount#account.balance,
+      {reply, io:format("Charged ~p Bambeuros, balance is now at ~p.~n", [Amount, NewBalance]), NewAccount};
+    false ->
+      {reply, 0, Account}
+  end;
+handle_call({withdraw, Amount}, _From, Account) when Amount =< 0 ->
+  {reply, 0, Account};
+handle_call({withdraw, Amount}, _From, Account = #account{balance = Balance, transactions = Transactions}) ->
+  case Balance > Amount of
+    true  ->
+      NewAccount = Account#account{balance = Balance - Amount, transactions = [{withdraw, Amount} | Transactions]},
+      NewBalance = NewAccount#account.balance,
+      {reply, io:format("Withdrawn ~p Bambeuros, balance is now at ~p.~n", [Amount, NewBalance]), NewAccount};
+    false ->
+      {reply, Balance, #account{}}
+  end;
+handle_call({deposit, Amount}, _From, Account) when Amount =< 0 ->
+  {reply, 0, Account};
+handle_call({deposit, Amount}, _From, Account = #account{balance = Balance, transactions = Transactions}) ->
+  NewAccount = Account#account{balance = Balance + Amount, transactions = [{deposit, Amount} | Transactions]},
+  NewBalance = NewAccount#account.balance,
+  {reply, io:format("Deposited ~p Bambeuros, balance is now at ~p.~n", [Amount, NewBalance]), NewAccount};
+handle_call(_Request, _From, State = #account{}) ->
   {reply, ok, State}.
 
 %% @private
 %% @doc Handling cast messages
--spec(handle_cast(Request :: term(), State :: #state{}) ->
-  {noreply, NewState :: #state{}} |
-  {noreply, NewState :: #state{}, timeout() | hibernate} |
-  {stop, Reason :: term(), NewState :: #state{}}).
-handle_cast(_Request, State = #state{}) ->
-  {noreply, State}.
+handle_cast(_Request, Account = #account{}) ->
+  {noreply, Account}.
 
 %% @private
 %% @doc Handling all non call/cast messages
--spec(handle_info(Info :: timeout() | term(), State :: #state{}) ->
-  {noreply, NewState :: #state{}} |
-  {noreply, NewState :: #state{}, timeout() | hibernate} |
-  {stop, Reason :: term(), NewState :: #state{}}).
-handle_info(_Info, State = #state{}) ->
-  {noreply, State}.
+handle_info(_Info, Account = #account{}) ->
+  {noreply, Account}.
 
 %% @private
 %% @doc This function is called by a gen_server when it is about to
 %% terminate. It should be the opposite of Module:init/1 and do any
 %% necessary cleaning up. When it returns, the gen_server terminates
 %% with Reason. The return value is ignored.
--spec(terminate(Reason :: (normal | shutdown | {shutdown, term()} | term()),
-    State :: #state{}) -> term()).
-terminate(_Reason, _State = #state{}) ->
+terminate(_Reason, _State = #account{}) ->
   ok.
 
 %% @private
-%% @doc Convert process state when code is changed
--spec(code_change(OldVsn :: term() | {down, term()}, State :: #state{},
-    Extra :: term()) ->
-  {ok, NewState :: #state{}} | {error, Reason :: term()}).
-code_change(_OldVsn, State = #state{}, _Extra) ->
-  {ok, State}.
-
-%%%===================================================================
-%%% Internal functions
-%%%===================================================================
+%% @doc Convert process account when code is changed
+code_change(_OldVsn, Account = #account{}, _Extra) ->
+  {ok, Account}.
